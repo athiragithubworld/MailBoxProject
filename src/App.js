fa9d3cf334c73ae46b069f9270eddf647db77475
athiragithubworld <athirahrms@gmail.com>
2023-07-31 00:01:36 +0530

    App js (Task - 3)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { useSelector } from "react-redux";
 import LoginPage from "./Components/Authentication/LoginPage";
 import Headers from "./Components/Layouts/Headers";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const auth = useSelector((state) => state.auth);
   return (
     <div>
-      {/* <Headers></Headers> */}
-      <LoginPage></LoginPage>
+      {auth.isLoggedIn && <Headers></Headers>}
+      <Routes>
+        {!auth.isLoggedIn && <Route path="/" element={<LoginPage />}></Route>}
+      </Routes>
     </div>
   );
 }
