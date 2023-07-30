bd4ac4df4854684f65966efb8e30f9196f6bed5b
athiragithubworld <athirahrms@gmail.com>
2023-07-30 11:21:13 +0530

    App js (Task -1)

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import LoginPage from "./Components/Authentication/LoginPage";
+import Headers from "./Components/Layouts/Headers";
+
+function App() {
+  return (
+    <div>
+      {/* <Headers></Headers> */}
+      <LoginPage></LoginPage>
+    </div>
+  );
+}
+
+export default App;
