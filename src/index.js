f52f1ea897bf66f3d72e5ffdf9deda13868c5ce9
athiragithubworld <athirahrms@gmail.com>
2023-07-31 00:01:14 +0530

    index js and css (Task -3)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./Store/ReduxIndex";
 
-import './index.css';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
