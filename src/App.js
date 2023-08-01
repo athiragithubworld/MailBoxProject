c8d1eefc5bb12207f4d0b160b8a64d29cee843eb
athiragithubworld <athirahrms@gmail.com>
2023-08-02 00:00:28 +0530

    App js - Task 5

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { useSelector } from "react-redux";
 import LoginPage from "./Components/Authentication/LoginPage";
 import Headers from "./Components/Layouts/Headers";
 import { Routes, Route } from "react-router-dom";
+import MainMenuList from "./Components/Layouts/MainMenuList";
+import ComposeMail from "./Components/Pages/ComposeMail";
 
 function App() {
   const auth = useSelector((state) => state.auth);
   return (
     <div>
-      {auth.isLoggedIn && <Headers></Headers>}
+      {auth.isLoggedIn && <Headers />}
+      {auth.isLoggedIn && <MainMenuList />}
+      {/* <ComposeMail /> */}
       <Routes>
         {!auth.isLoggedIn && <Route path="/" element={<LoginPage />}></Route>}
       </Routes>
