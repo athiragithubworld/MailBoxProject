ddb99f697b01792a174527783dcc2bdc087a6d00
athiragithubworld <athirahrms@gmail.com>
2023-08-03 10:09:36 +0530

    ReduxIndex js (Task-7) Inbox view store

--- src/Store/ReduxIndex.js
+++ src/Store/ReduxIndex.js
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./AuthSlice";
+import mailBoxReducer from "./MailBoxSlice";
+import inboxReducer from "./InboxSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
+    mailBoxData: mailBoxReducer,
+    inboxMail: inboxReducer,
   },
 });
 
