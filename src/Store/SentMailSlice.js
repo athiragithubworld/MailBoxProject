42346e66bcf935ffcfa7e88e393d29a075243474
athiragithubworld <athirahrms@gmail.com>
2023-08-05 01:18:13 +0530

    SentMailSlice js (Task - 13) SentMail List and ViewMail

--- src/Store/SentMailSlice.js
+++ src/Store/SentMailSlice.js
@@ -1,13 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { mailItems: [], totalInboxMail: 0, totalSentMail: 0 };
+const initialState = {
+  sentMailList: [],
+  viewSentMail: JSON.parse(localStorage.getItem("viewSentMail")),
+};
 
 const SentMailSlice = createSlice({
   name: "sentMail",
   initialState,
   reducers: {
-    sentMail(state, action) {
-      state.mailItems = [action.payload, ...state.mailItems];
+    sentedMails(state, action) {
+      state.sentMailList = action.payload;
+    },
+    viewSentMail(state, action) {
+      state.viewSentMail = action.payload;
+      localStorage.setItem("viewSentMail", JSON.stringify(action.payload));
+    },
+    deleteSentMail(state, action) {
+      state.sentMailList = state.sentMailList.filter(
+        (item) => item.id !== action.payload.id
+      );
     },
   },
 });
