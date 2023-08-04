013a04e895f54931ccb5ef518fcfec400477b4b6
athiragithubworld <athirahrms@gmail.com>
2023-08-04 18:11:09 +0530

    DraftMail js  (Task -9) Unread Msg

---
+++ src/Components/Pages/DraftMail.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const DraftMail = () => {
+  return (
+    <div>
+      <div
+        className="container-sm"
+        style={{
+          backgroundColor: "white",
+          marginTop: "10px",
+          marginLeft: "50px",
+          marginRight: "10px",
+          marginBottom: "5px",
+        }}
+      >
+        <h1>Draft Mails</h1>
+      </div>
+    </div>
+  );
+};
+
+export default DraftMail;
