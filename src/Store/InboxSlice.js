54d968f334cf50c326d11bb085cab2f8b38b89e0
athiragithubworld <athirahrms@gmail.com>
2023-08-05 01:17:07 +0530

    InboxSlice Js - Some update

--- src/Store/InboxSlice.js
+++ src/Store/InboxSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   inboxMails: [],
-  totalRecieveMail: 0,
+  // totalRecieveMail: 0,
   unread: true,
   viewMail: JSON.parse(localStorage.getItem("viewMail")),
 };
