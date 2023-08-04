86d4c27b93c9dccb3f9e579565d1c3a1b51c7744
athiragithubworld <athirahrms@gmail.com>
2023-08-04 18:12:47 +0530

    ReduxIndex js  (Task -9) Unread Msg

--- src/Store/ReduxIndex.js
+++ src/Store/ReduxIndex.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./AuthSlice";
-import mailBoxReducer from "./MailBoxSlice";
+// import mailBoxReducer from "./MailBoxSlice";
 import inboxReducer from "./InboxSlice";
+import sentMailReducer from "./SentMailSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
-    mailBoxData: mailBoxReducer,
+    // mailBoxData: mailBoxReducer,
     inboxMail: inboxReducer,
+    sentMail: sentMailReducer,
   },
 });
 
